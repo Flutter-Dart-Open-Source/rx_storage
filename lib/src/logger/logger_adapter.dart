d666421b74b1d282783508446de2801c46639c25
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2020-12-27 00:23:28 +0700

    wip

--- lib/src/logger/logger_adapter.dart
+++ lib/src/logger/logger_adapter.dart
@@ -7,13 +7,13 @@ class LoggerAdapter implements Logger {
   const LoggerAdapter();
 
   @override
-  void doOnDataStream(KeyAndValue pair) {}
+  void doOnDataStream(KeyAndValue<dynamic, dynamic> pair) {}
 
   @override
   void doOnErrorStream(Object error, StackTrace stackTrace) {}
 
   @override
-  void keysChanged(Iterable<KeyAndValue> pairs) {}
+  void keysChanged(Iterable<KeyAndValue<dynamic, dynamic>> pairs) {}
 
   @override
   void readValue(Type type, Object key, dynamic value) {}
