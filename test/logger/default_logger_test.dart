9580d7cc3e3aee98f142573a0fe71fc5ddc76075
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-02-14 21:03:31 +0700

    :white_check_mark: Adding tests.

--- test/logger/default_logger_test.dart
+++ test/logger/default_logger_test.dart
@@ -62,4 +62,65 @@ void main() {
       );
     });
   });
+
+  group('DefaultLogger<String, int>', () {
+    final logger = DefaultLogger<String, int>();
+
+    test('KeysChangedEvent', () {
+      const pairs = [
+        KeyAndValue('key1', 'value1'),
+        KeyAndValue('key2', 2),
+      ];
+
+      expect(
+        () => logger.log(KeysChangedEvent(pairs)),
+        prints([
+          ' ↓ Key changes',
+          "    → { 'key1': value1 }",
+          "    → { 'key2': 2 }\n",
+        ].join('\n')),
+      );
+      ;
+    });
+
+    test('OnDataStreamEvent', () {
+      const pair = KeyAndValue('key1', 'value1');
+      expect(
+        () => logger.log(OnDataStreamEvent(pair)),
+        prints(" → Stream emits data: { 'key1': value1 }\n"),
+      );
+    });
+
+    test('OnErrorStreamEvent', () {
+      final stackTrace = StackTrace.current;
+      final exception = Exception();
+      expect(
+        () => logger
+            .log(OnErrorStreamEvent(RxStorageError(exception, stackTrace))),
+        prints(' → Stream emits error: $exception, $stackTrace\n'),
+      );
+    });
+
+    test('ReadValueSuccessEvent', () {
+      const type = String;
+      const key = 'key';
+      const value = 'value';
+      expect(
+        () =>
+            logger.log(ReadValueSuccessEvent(KeyAndValue(key, value), type, 0)),
+        prints(" → Read: type=String, key='key', options=0 → value\n"),
+      );
+    });
+
+    test('WriteSuccessEvent', () {
+      const type = String;
+      const key = 'key';
+      const value = 'value';
+      expect(
+        () => logger.log(WriteSuccessEvent(KeyAndValue(key, value), type, 0)),
+        prints(
+            " ← Write: key='key', value=value, type=String, options=0 → success\n"),
+      );
+    });
+  });
 }
